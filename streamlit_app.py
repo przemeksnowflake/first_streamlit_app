092b9af5090c5ad5ba0fa815313e9f2c65a0476c
przemeksnowflake <111129101+przemeksnowflake@users.noreply.github.com>
2022-08-16 09:46:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('Something')
 streamlit.text('text something')
 streamlit.text('🥣 🥗 🐔 🥑🍞 some emojis yo')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
