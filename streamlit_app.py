3c21761622d3db44dac0cef05a01dec8d63ab52a
przemeksnowflake <111129101+przemeksnowflake@users.noreply.github.com>
2022-08-16 09:50:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.text('text something')
 streamlit.text('🥣 🥗 🐔 🥑🍞 some emojis yo')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list))
 streamlit.dataframe(my_fruit_list)
